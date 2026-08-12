4e440cfc2974f8f0a6dbaa5d33dd7f92a7dc0292
Mari <146585679+Marisftwr@users.noreply.github.com>
2026-08-12 16:22:43 -0300

    Delete Django-API-Start/mymovie directory

--- Django-API-Start/mymovie/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MymovieConfig(AppConfig):
-    name = 'mymovie'
--- Django-API-Start/mymovie/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
-    path("blogposts/<int:pk>/", views.BlogPostRetrieveUpdateDestroy.as_view(), name = "updates"),
-    path("blogposts/", views.BlogPostList.as_view(), name = "costumize" )
-]
